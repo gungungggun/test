5db5a7a102f4f6805032e931e0887f2be2c0aa5c
gun <gungungggun@gmail.com>
2020-03-17 14:57:52 +0900

    moment

--- simple-vue/src/App.vue
+++ simple-vue/src/App.vue
@@ -1,21 +1,44 @@
 <template lang="pug">
   div#app
-    img(alt="Vue logo" src="./assets/logo.png")
+    full-page(ref="fullpage" :options="options")
+      .section
+        Date(:date="date")
+      .section
+        p Second section ...
+        button(@click="add()") add
+      .section(v-if="page")
+        p Hello
 </template>
 
 <script>
+import Date from '@/components/Date'
+import moment from 'moment'
+
 export default {
-  name: 'App'
+  components: {
+    Date
+  },
+  data() {
+    return {
+      date: moment('20200320'),
+      page: false,
+      options: {
+        v2compatible: true,
+        sectionsColor: ['#000', '#ff5f45', '#0798ec']
+      }
+    }
+  },
+  methods: {
+    add() {
+      this.page = true
+      this.$refs.fullpage.build()
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+body, html
+  margin 0
+  padding 0
 </style>
---
+++ simple-vue/src/components/Date.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+.date
+  span.year {{ date.format('YYYY') }}
+  span.dot .
+  span.month {{ date.format('MM') }}
+  span.dot .
+  span.day {{ date.format('DD') }}
+  span {{ date.format('ddd').toUpperCase() }}
+</template>
+
+<script>
+export default {
+  props: {
+    date: Object
+  }
+}
+</script>
+
+<style lang="stylus">
+.date
+  span
+    font-size 30px
+    color #fff
+    padding 0 2px
+</style>
